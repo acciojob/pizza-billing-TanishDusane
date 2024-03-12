fd9f0d5ec9afe97c843a8a5acdd8c09f4770c64d
Tanish Dusane <91111180+DeathxHunter@users.noreply.github.com>
2024-03-12 20:21:42 +0530

    Pizza Bill Generator Assignment

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,17 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean extraCheeseAdded;
+    private boolean extraToppingAdded;
+    private boolean paperbagAdded;
+    private int basePrice;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.basePrice = isVeg ? 300 : 400;
+        this.price = basePrice;
+        this.bill = "";
     }
 
     public int getPrice(){
@@ -17,18 +24,37 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!extraCheeseAdded) {
+            extraCheeseAdded = true;
+            price += 80;
+            this.bill += "Extra Cheese Added: " + 80 + "\n";
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!extraToppingAdded) {
+            extraToppingAdded = true;
+            price += isVeg ? 70 : 120;
+            this.bill += "Extra Topping Added: " + (isVeg ? 70 : 120) + "\n";
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!paperbagAdded) {
+            paperbagAdded = true;
+            price += 20;
+            this.bill += "Paperbag Added: " + 20 + "\n";
+        }
     }
 
     public String getBill(){
+        StringBuilder finalbill = new StringBuilder();
+        finalbill.append("Base Price Of The Pizza: ").append(basePrice).append("\n");
+        finalbill.append(bill);
+        finalbill.append("Total Price: ").append(price);
         // your code goes here
-        return this.bill;
+        return finalbill.toString();
     }
 }
